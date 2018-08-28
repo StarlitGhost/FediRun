bade13992f0f79394c396e3407e0cfa6829e2824
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-08-29 00:04:13 +0100

    check the exit code to determine if errors need to be output, comment everything

--- FediRun.py
+++ FediRun.py
@@ -12,41 +12,55 @@ class FediRun(PineappleBot):
     def respond(self, status, user):
         username = user["acct"]
 
+        # decode the toot into raw text
         soup = BeautifulSoup(status["content"], "lxml")
+        # strip mentions
         [mention.extract() for mention in soup.find_all(class_="h-card")]
+        # replace <br /> with newlines
         [br.replace_with('\n') for br in soup.find_all("br")]
+
         lines = soup.text.splitlines()
 
+        # the language must be on the first line of the toot
         language = lines[0].strip()
-
         if not language:
             self._send_reply('@{} the language name *must* be on the first line of your toot'.format(username), status)
             return
+        # check if tio.run accepts this language
+        if language.lower() in self.languages_friendly:
+            # convert friendly language name to api language name
+            language = self.languages_friendly[language.lower()]
+        if language.lower() not in self.languages:
+            # no match; fetch a list of close language name matches
+            lang_list = self._closest_matches(language, self.languages_friendly.keys(), 10, 0.8)
+            lang_string = "\n".join(lang_list)
+            self._send_reply('@{} language {!r} is unknown on https://tio.run\n'.format(username, language) +
+                             'Perhaps you wanted one of these?\n\n' +
+                             '{}'.format(lang_string),
+                             status)
+            return
 
+        # the rest of the toot is treated as code
         code = '\n'.join(lines[1:])
 
+        # send the code off to tio.run
         returned, errors = self._tio(language, code)
 
         response = '@{} {}'.format(username, returned)
-        if errors:
+        # grab and check the exit code
+        if int(errors.splitlines()[-1][len("Exit code: ")-1:]) != 0:
+            # add error output if the exit code was non-zero
             response += '\nError: {}'.format(errors)
 
         self._send_reply(response, status)
 
     def _tio(self, language, code, user_input=''):
-        if language.lower() in self.languages_friendly:
-            language = self.languages_friendly[language.lower()]
-        if language.lower() not in self.languages.keys():
-            lang_list = self._closest_matches(language.lower(), self.languages.keys(), 10, 0.8)
-            lang_string = "\n".join(lang_list)
-            return ("", "language {!r} is unknown on tio.run.\n".format(language) +
-                    "Perhaps you wanted one of these?\n\n" +
-                    "{}".format(lang_string))
-
+        # build our request dictionary
         request = [{'command': 'V', 'payload': {'lang': [language.lower()]}},
                    {'command': 'F', 'payload': {'.code.tio': code}},
                    {'command': 'F', 'payload': {'.input.tio': user_input}},
                    {'command': 'RC'}]
+        # convert the dictionary into the form tio.run accepts
         req = b''
         for instr in request:
             req += instr['command'].encode()
@@ -61,8 +75,10 @@ class FediRun(PineappleBot):
                 req += value
         req_raw = zlib.compress(req, 9)[2:-4]
 
+        # send our request off to tio.run
         url = "https://tio.run/cgi-bin/static/b666d85ff48692ae95f24a66f7612256-run/93d25ed21c8d2bb5917e6217ac439d61"
         res = requests.post(url, data=req_raw)
+        # decode the results
         res = zlib.decompress(res.content, 31)
         delim = res[:16]
         ret = res[16:].split(delim)
@@ -77,7 +93,9 @@ class FediRun(PineappleBot):
         return return_str, error_str
 
     def start(self):
+        # fetch language list from tio.run
         self.languages = self._fetch_languages()
+        # build a mapping of friendly language names to api language names
         self.languages_friendly = {d['name'].lower(): l for l, d in self.languages.items()}
 
     def _fetch_languages(self):
@@ -87,6 +105,8 @@ class FediRun(PineappleBot):
         return response.json()
 
     def _closest_matches(self, search: str, word_list: List[str], num_matches: int, threshold: float) -> List[str]:
+        # measure the Damerau-Levenshtein distance between our target word and a list of possible close matches,
+        # returning up to num_matches results
         similarities = sorted([(ndld(search, word), word) for word in word_list])
         close_matches = [word for (diff, word) in similarities if diff <= threshold]
         top_n = close_matches[:num_matches]
